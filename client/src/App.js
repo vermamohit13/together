a8e5b62f9dc3685bc03ebcef91464814ede2ca43
palaksharma <b20159@students.iitmandi.ac.in>
2022-02-26 22:42:44 +0530

    home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Ho from "./components/About";
+
 import './App.css';
-import React from 'react';
-import Home from './components/pages/home';
+import Home from './components/Home';
 import Login from './components/pages/login';
 import Signup from './components/pages/signup';
 import PrivateRoute from './components/privateroute';
 import Dashboard from './components/pages/dashboard';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
 function App() {
   return (
     <>
@@ -15,6 +26,18 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/home" element={<PrivateRoute />}>
               <Route exact path="/home" element={<Dashboard />} />
+            </Route>
+            <Route exact path="/mycommunity" element={<PrivateRoute />}>
+              <Route exact path="/mycommunity" element={<Dashboard />} />
+            </Route>
+            <Route exact path="/mycommunity" element={<PrivateRoute />}>
+              <Route exact path="/mycommunity" element={<Dashboard />} />
+            </Route>
+            <Route exact path="/search" element={<PrivateRoute />}>
+              <Route exact path="/search" element={<Dashboard />} />
+            </Route>
+            <Route exact path="/invitation" element={<PrivateRoute />}>
+              <Route exact path="/invitation" element={<Dashboard />} />
             </Route>
             <Route exact path="/login" element={<Login />} />
 
